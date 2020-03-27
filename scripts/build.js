af1ef4cff31a10c0bf92b250553037a54387d9da
Bryan Horna <bryanjhv@gmail.com>
2020-03-27 00:04:59 -0500

    Setup website building/processing

---
+++ scripts/build.js
@@ -0,0 +1,21 @@
+const { minify } = require('html-minifier');
+const { dirname, extname } = require('path');
+const { copySync, removeSync } = require('fs-extra');
+const { readFileSync, writeFileSync } = require('fs');
+
+process.chdir(dirname(__dirname));
+
+const htmlMinOpts = JSON.parse(readFileSync('.htmlminrc', 'utf8'));
+
+removeSync('public');
+copySync('src', 'public', {
+  filter(src, out) {
+    if (extname(src) == '.html')
+      return writeFileSync(
+        out,
+        minify(readFileSync(src, 'utf8'), htmlMinOpts),
+        'utf8'
+      );
+    return true;
+  },
+});
